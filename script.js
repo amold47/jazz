c67e7b086fab1e3491af2a8056c0e14c5cb2679a
Andy Mold <andy.mold47@gmail.com>
2022-12-13 16:46:56 -0500

    Final Additions

---
+++ script.js
@@ -0,0 +1,3 @@
+function displayConfirmation() {
+    document.getElementById("Confirmation").innerHTML = "Thank you for submitting the form! We will call within 48 hours to determine further details.";
+}
